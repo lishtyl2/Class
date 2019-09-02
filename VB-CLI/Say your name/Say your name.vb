58440fadff60e4d40b50d439deee1b121a1db924
lishtyl2 <54544221+lishtyl2@users.noreply.github.com>
2019-09-01 21:45:44 -0600

    Say your name

---
+++ VB-CLI/Say your name/Say your name.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Message, Title, Myvalue
+        Message = "Hello! What's your name?"
+        Title = "Name asker"
+        Myvalue = InputBox(Message, Title)
+        If Myvalue = "Emily" Then
+            Console.WriteLine("Hello Emily")
+        ElseIf Myvalue = "Joe" Then
+            Console.WriteLine("Hello Joe")
+        ElseIf Myvalue = "Tyler" Then
+            Console.WriteLine("AWWWW Let's get the party started!")
+        End If
+
+    End Sub
+
+End Module
